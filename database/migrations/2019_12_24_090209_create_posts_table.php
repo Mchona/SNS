dd2d6375a4e82f0c3440ff9edacf61fbf12e2bd4
Mchona <masaru.chona@gmail.com>
2023-03-30 11:20:36 +0900

    新規登録パスワードまで

--- database/migrations/2019_12_24_090209_create_posts_table.php
+++ database/migrations/2019_12_24_090209_create_posts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -16,7 +17,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->integer('id')->autoIncrement();
             $table->integer('user_id');
-            $table->string('post',400);
+            $table->string('post', 400);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('current_timestamp on update current_timestamp'));
         });
